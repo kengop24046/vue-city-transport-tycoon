36a6e6aad686b9720c74f09d835fdd03fbdf5082
kengop24046 <kengop8888@icloud.com>
2026-03-04 01:39:45 +0800

    fix: 修正base路径配置

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const GITHUB_REPO_NAME = 'vue-city-transport-tycoon'
-
 export default defineConfig({
   plugins: [vue()],
-  base: `/vue-city-transport-tycoon/`,
-  server: {
-    port: 3000,
-    open: true,
-    host: '0.0.0.0',
-  },
+  base: '/vue-city-transport-tycoon/', 
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    emptyOutDir: true
   }
 })
